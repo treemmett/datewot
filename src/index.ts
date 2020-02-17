165fcd83b1782c7d69569ceb2c5430e95891507f
Tregan Emmett <tregan@tregan.me>
2020-02-16 22:20:25 -0800

    feat: add datewot class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-export default function(): string {
-  return 'hi';
+export default class Datewot {
+  private year: number;
+  private month: number;
+  private date: number;
+
+  public constructor() {
+    const now = new Date();
+    this.year = now.getFullYear();
+    this.month = now.getMonth();
+    this.date = now.getDate();
+  }
+
+  public getFullYear(): number {
+    return this.year;
+  }
+
+  public getMonth(): number {
+    return this.month;
+  }
+
+  public getDate(): number {
+    return this.date;
+  }
 }
